ad91e507e559e0fc217f0286f8c5cbed978cc306
lilliputten <lilliputten@gmail.com>
2024-08-28 01:04:34 +0300

    Stopped at...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
+// Stopped at 2024.08.28, 01:04: https://vercel.com/guides/nextjs-prisma-postgres#step-8.-add-new-post-functionality
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
